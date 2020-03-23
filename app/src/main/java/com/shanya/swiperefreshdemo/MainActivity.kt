8b613225a027b5f03c7abbb9da951cb9a2397e4c
Shanyaliux <445502929@qq.com>
2020-03-23 08:18:54 +0800

    Initial commit

---
+++ app/src/main/java/com/shanya/swiperefreshdemo/MainActivity.kt
@@ -0,0 +1,19 @@
+package com.shanya.swiperefreshdemo
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
+import java.util.*
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        swipeRedreshLayout.setOnRefreshListener {
+            textView.text = System.currentTimeMillis().toString()
+            swipeRedreshLayout.isRefreshing = false
+        }
+    }
+}
